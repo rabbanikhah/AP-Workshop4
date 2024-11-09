d9fb33988e960243a4af9ef8985e2e83dc53ad2a
rabbanikhah <rabbanikhah.neda@gmail.com>
2024-11-09 21:17:32 +0330

    added getters to person class

--- src/Person.java
+++ src/Person.java
@@ -2,10 +2,19 @@ public class Person {
     // field and attribute
     private String firstName;
     private String lastName;
-    
+
     // constructor
     public Person(String firstName, String lastName) {
         this.firstName = firstName;
         this.lastName = lastName;
+    }
+
+    // methods
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
     }
 }
