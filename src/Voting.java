7706102f785b0abe48ce9c2370d42f9f410c6322
rabbanikhah <rabbanikhah.neda@gmail.com>
2024-11-09 21:31:27 +0330

    added fields to voting

--- src/Voting.java
+++ src/Voting.java
@@ -1,2 +1,12 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class Voting {
+    // field and attribute
+    private int type;
+    private String question;
+    private HashMap<String, HashSet<Vote>> choices;
+    private boolean isAnonymous;
+    private ArrayList<Person> voters;
 }
