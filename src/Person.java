440dc66f6b2c7c8a8cff6e6b4763e6081cf63ea6
rabbanikhah <rabbanikhah.neda@gmail.com>
2024-11-09 21:17:10 +0330

    added constructor person class

--- src/Person.java
+++ src/Person.java
@@ -2,4 +2,10 @@ public class Person {
     // field and attribute
     private String firstName;
     private String lastName;
+    
+    // constructor
+    public Person(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
 }
