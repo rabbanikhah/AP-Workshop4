89e6de9e718b695e08e47ad28834617697e1a78c
rabbanikhah <rabbanikhah.neda@gmail.com>
2024-11-09 21:20:48 +0330

    added constructor to vote class

--- src/Vote.java
+++ src/Vote.java
@@ -2,4 +2,10 @@ public class Vote {
     // field and attribute
     private final Person voter;
     private final String date;
+    
+    // constructor
+    public Vote(Person voter, String date) {
+        this.voter = voter;
+        this.date = date;
+    }
 }
