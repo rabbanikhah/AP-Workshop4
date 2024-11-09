8e30f2ca0374045a3b470c4cd9427853870a7d57
rabbanikhah <rabbanikhah.neda@gmail.com>
2024-11-09 21:21:37 +0330

    added getters to vote class

--- src/Vote.java
+++ src/Vote.java
@@ -2,10 +2,19 @@ public class Vote {
     // field and attribute
     private final Person voter;
     private final String date;
-    
+
     // constructor
     public Vote(Person voter, String date) {
         this.voter = voter;
         this.date = date;
+    }
+
+    // methods
+    public Person getVoter() {
+        return voter;
+    }
+
+    public String getDate() {
+        return date;
     }
 }
